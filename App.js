dadd80395a802548a8cf2a360f0e23ce513e983f
satyamsb1 <satyamborade45@gmail.com>
2023-07-14 23:31:23 +0530

    Updated the notes and started building food delivery app

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Title= () => (
-    <h1 id="heading">
-        This is the jsx heading Title
-    </h1>
-);
+const Header = () =>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-const HeadingComponent = () =>(
-    <div className="container">
-        <Title/>    
-        <h1>This is a react functional Component</h1>
-    </div>
-);
- 
+const RestaurantCard = () =>{
+    return(
+        <div className="res-card">
+            <h3>Meghana Foods</h3>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+                <div className="res-container">
+                   <RestaurantCard/>
+                </div>
+        </div>
+    )
+}
+
+const AppLayout= () =>{
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
 /*
 Component can also be written like this;
 1. const HeadingComponent = () => <h1>This is a react functional Component</h1>;  
