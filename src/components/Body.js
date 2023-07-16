ab49536a4fd4c963e828ef1f897b0cdfcbbe0d48
satyamsb1 <satyamborade45@gmail.com>
2023-07-17 00:38:33 +0530

    Filtered out top rated restaurants using react useState() hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
+
+
 const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
     return (
         
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" 
                 onClick= {()=>{
-                    console.log("Button Clicked")
+                    //Filter code here 
+                    const fileredList = listOfRestaurants.filter((res)=>res.data.avgRating > 4
+                    );
+                    setListOfRestaurants(fileredList);
+                    console.log(listOfRestaurants);
                 }}>
                     Top Rated Restaurants
                 </button>
             </div>
                 <div className="res-container">
-                   {resList.map(restaurant => <RestaurantCard key = {restaurant.data.id}  resData= {restaurant}/>)}
+                   {listOfRestaurants.map(restaurant => <RestaurantCard key = {restaurant.data.id}  resData= {restaurant}/>)}
                 </div>
         </div>
     );
